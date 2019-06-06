48a31ec2cc9df874ea37320fc864a19650d2376c
Charlie Harding <xsanda@me.com>
2019-06-06 03:19:20 +0200

    Add autogeneration of web pages

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+gem 'github-pages', group: :jekyll_plugins
+
+gem "bundler", "~> 2.0"
+gem "nokogiri", "~> 1.10"
+gem "maruku", "~> 0.7.3"
+gem "json", "~> 2.2"
+gem "haml", "~> 5.1"
+gem "fastimage", "~> 2.1"
+
+gem "haml-contrib", "~> 1.0"
---
+++ build.rb
@@ -0,0 +1,141 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+require 'open-uri'
+require 'nokogiri'
+require 'haml'
+require 'ostruct'
+require 'fastimage'
+require 'json'
+
+Haml::TempleEngine.options[:attr_wrapper] = '"'
+
+class Template
+  def initialize(template)
+    @template = File.read template
+  end
+
+  def build_page(properties = {})
+    Haml::Engine.new(@template).to_html(OpenStruct.new(properties))
+  end
+end
+
+class Hike
+  @@template = Template.new 'templates/hike.haml'
+  
+  def initialize(yaml, link)
+    @link = link
+    @id = yaml['id']
+    @desc = yaml['desc']
+    fetch_info
+    fetch_participants
+  end
+
+  attr_reader :id, :desc, :tags, :image, :title, :date, :link
+  attr_reader :capacity, :registered, :waiting
+
+  def url
+    hb "/routes/events/#{@id}/"
+  end
+
+  def local_link
+    "/#{link}"
+  end
+
+  def page_title
+    "#{date_string}: #{title} - Hiking Buddies Munich"
+  end
+
+  def date_string
+    date.strftime('%-d %b')
+  end
+
+  def time_string
+    date.strftime('%H:%M')
+  end
+
+  def available
+    capacity - registered - waiting
+  end
+
+  def image_height
+    fetch_image_info unless instance_variable_defined? :@image_height
+    @image_height
+  end
+
+  def image_width
+    fetch_image_info unless instance_variable_defined? :@image_width
+    @image_width
+  end
+
+  def save
+    html = @@template.build_page(
+      url: url, title: page_title,
+      image: image, image_width: image_width, image_height: image_height)
+    File.write("#{link}.html", html)
+  end
+
+  def self.save_index(hikes)
+    template = Template.new('templates/listing.haml')
+    html = template.build_page(hikes: hikes.sort_by(&:date))
+    File.write('index.html', html)
+  end
+
+  private
+
+  def fetch_info
+    doc = Nokogiri::HTML source(url)
+    
+    parse_title_tags doc.at('.event-name').text
+    
+    rel_image = doc.at('.cover_container')['style'].match(/url\((.+)\)/)[1]
+    @image = URI::join(url, rel_image).to_s
+    
+    date_string = doc.at('input[name=start]')["value"]
+    @date = DateTime.strptime(date_string, "%m/%d/%Y %H:%M:%S")
+
+    @capacity = doc.at('input[name=max_participants]')["value"].to_i
+  end
+  
+  def fetch_participants
+    json = JSON.parse(source(hb "/routes/get_event_details/?event_id=#{@id}"))
+    @registered = JSON.parse(json['participants']).length
+    @waiting = JSON.parse(json['participants_waiting']).length
+  end
+  
+  def source(url)
+    open(url, 'Accept-Language' => 'en') { |f| f.read }
+  end
+
+  def parse_title_tags raw_title
+    tags = []
+    loop do
+      match = raw_title.match(/^\[(.+?)\]\s*(.+)$/)
+      break unless match
+      tags << match[1]
+      raw_title = match[2]
+    end
+    @title = raw_title
+    @tags = tags
+    return @title, @tags
+  end
+
+  def hb url
+    URI::join('https://www.hiking-buddies.com/', url).to_s
+  end
+
+  def fetch_image_info
+    @image_width, @image_height = FastImage.size(image)
+  end
+end
+
+if __FILE__ == $0
+  hikes = YAML.load_file('hikes.yml').map do |link, hike|
+    raise 'Cannot overwrite index' if link == 'index'
+    hike = Hike.new(hike, link)
+    hike.save
+    hike
+  end
+
+  Hike.save_index(hikes)
+end
